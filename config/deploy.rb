a52edede899f69f4384bf00343c93ce8ad1036db
d1mentor <gerasimenkot92@gmail.com>
2022-09-04 21:22:10 +0300

    fix deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
+set :ssh_options, {verify_host_key: :never}
 
 # Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
 # This is useful if you don't want to use ENV variables
