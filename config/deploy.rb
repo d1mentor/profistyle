24f5928807e7aca19c8825518153790b1c4ef33a
d1mentor <gerasimenkot92@gmail.com>
2022-09-04 23:28:25 +0300

    Я не здамся

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,9 @@ set :linked_files, %w{config/master.key}
 # Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
-
+append :linked_files, 'config/database.yml', 'config/credentials.yml.enc', 'config/master.key'
+      append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle',
+                           'public/system', 'public/uploads'
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
 set :ssh_options, {verify_host_key: :never}
